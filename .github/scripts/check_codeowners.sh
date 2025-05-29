29055af6703b0cb05a71a4fc5e6ce8db8cb83d04
Paul Fitzgerald <pafitzge@redhat.com>
2025-05-29 14:29:05 +0100

    Add check_codeowners workflow

---
+++ .github/scripts/check_codeowners.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+# Usage: ./check_codeowners.sh mydir/tasks/apply-mapping some/other/dir
+
+if [ $# -gt 0 ]
+then
+  DIRECTORIES=$@
+fi
+
+if [ -z "${DIRECTORIES}" ]
+then
+  echo Error: No directories as argument.
+  echo Usage:
+  echo "$0 [item1] [item2] [...]"
+  exit 1
+fi
+
+# check every item is a directory
+for DIR in $DIRECTORIES
+do
+  if [[ -d "$DIR" ]]; then
+    true
+  else
+    echo "Error: Not a directory: $ITEM"
+    exit 1
+  fi
+done
+
+for DIR in $DIRECTORIES
+do
+
+  SHORT_DIR=$(echo $DIR | cut -d '/' -f -2)
+  CODEOWNERS=${SHORT_DIR}/CODEOWNERS
+
+  if [ ! -f $CODEOWNERS ]
+  then
+    echo Error: CODEOWNERS file does not exist: $SHORT_DIR
+    exit 1
+  fi
+done
